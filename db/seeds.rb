95a44301e4d8a5a4986c2b0ce4664650d133bbf1
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 15:03:19 -0700

    Start adding some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+normal = Type.create!(name: 'normal')
+fighting = Type.create!(name: 'fighting')
+flying = Type.create!(name: 'flying')
+poison = Type.create!(name: 'poison')
+ground = Type.create!(name: 'ground')
+rock = Type.create!(name: 'rock')
+bug = Type.create!(name: 'bug')
+ghost = Type.create!(name: 'ghost')
+steel = Type.create!(name: 'steel')
+fire = Type.create!(name: 'fire')
+water = Type.create!(name: 'water')
+grass = Type.create!(name: 'grass')
+electric = Type.create!(name: 'electric')
+psychic = Type.create!(name: 'psychic')
+ice = Type.create!(name: 'ice')
+dragon = Type.create!(name: 'dragon')
+dark = Type.create!(name: 'dark')
+fairy = Type.create!(name: 'fairy')
+
+bulbasaur = Pokemon.create!(
+  name: 'Bulbasaur',
+  number: '001',
+  description: "Bulbasaur can be seen napping in bright sunlight. There is a seed on its back. By soaking up the sun's rays, the seed grows progressively larger."
+)
+
+bulbasaur.types << [grass, poison]
+bulbasaur.weaknesses << [fire, flying, ice, psychic]
+bulbasaur.strenghts << [water, fairy]
