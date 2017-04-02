bdf40438dab71054b73fa77c6a2d0cda609ddc8c
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 16:23:11 -0700

    Remove autoload

--- config/application.rb
+++ config/application.rb
@@ -26,10 +26,5 @@ module GraphqlPokemon
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.autoload_paths += [
-      "#{config.root}/app/graph",
-      "#{config.root}/app/graph/types"
-    ]
   end
 end
