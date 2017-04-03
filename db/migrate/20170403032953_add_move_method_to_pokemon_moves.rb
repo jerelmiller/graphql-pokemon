c917e6b6aeed5d609ac6db5842cf7968be4030bc
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 23:17:43 -0700

    Remove unique index that is causing problems on down

--- db/migrate/20170403032953_add_move_method_to_pokemon_moves.rb
+++ db/migrate/20170403032953_add_move_method_to_pokemon_moves.rb
@@ -2,9 +2,5 @@ class AddMoveMethodToPokemonMoves < ActiveRecord::Migration[5.0]
   def change
     add_reference :pokemon_moves, :move_method, index: true, null: false, foreign_key: true
     remove_index :pokemon_moves, [:pokemon_id, :move_id]
-    add_index :pokemon_moves,
-      [:pokemon_id, :move_id, :move_method_id],
-      unique: true,
-      name: 'index_pokemon_move_pokemon_move'
   end
 end
