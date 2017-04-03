0ca2193b377d3295a62560db3dbb07918062291c
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 20:55:24 -0700

    Update seeds to create moves for pokemon

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,22 @@
 pokemon_file = Rails.root.join('lib/pokemon.json')
 pokemons = JSON.parse(File.read(pokemon_file))
+pokemons = pokemons.map(&:with_indifferent_access)
 
-pokemons.each do |pokemon_json|
-  ActiveRecord::Transaction do
-    pokemon_json = pokemon_json.with_indifferent_access
 
+ActiveRecord::Base.transaction do
+  mapped_effects = {}
+  cached_types = {}
+  mapped_move_methods = {}
+
+  pokemons.flat_map do |pokemon_json|
+    pokemon_json['types'] +
+      pokemon_json['strengths'] +
+      pokemon_json['weaknesses']
+  end
+    .uniq
+    .each { |name| cached_types[name] = Type.create!(name: name) }
+
+  pokemons.each do |pokemon_json|
     pokemon = Pokemon.create!(
       name: pokemon_json[:name],
       number: pokemon_json[:number],
@@ -18,18 +30,48 @@ pokemons.each do |pokemon_json|
     )
 
     pokemon_json[:types].each do |type|
-      type = Type.where(name: type).first_or_create!
-      PokemonType.create!(pokemon: pokemon, type: type)
+      PokemonType.create!(pokemon: pokemon, type: cached_types[type])
     end
 
     pokemon_json[:weaknesses].each do |type|
-      type = Type.where(name: type).first_or_create!
-      PokemonWeakness.create!(pokemon: pokemon, type: type)
+      PokemonWeakness.create!(pokemon: pokemon, type: cached_types[type])
     end
 
     pokemon_json[:strengths].each do |type|
-      type = Type.where(name: type).first_or_create!
-      PokemonStrength.create!(pokemon: pokemon, type: type)
+      PokemonStrength.create!(pokemon: pokemon, type: cached_types[type])
+    end
+
+    pokemon_json[:moves].each do |move_json|
+      mapped_effects[move_json[:effect][:id]] ||=
+        Effect.create!(description: move_json[:effect][:description])
+
+      mapped_move_methods[move_json[:move_method][:id]] ||=
+        MoveMethod.create!(
+          name: move_json[:move_method][:name],
+          description: move_json[:move_method][:description]
+        )
+
+      effect = mapped_effects[move_json[:effect][:id]]
+      move_method = mapped_move_methods[move_json[:move_method][:id]]
+
+      move =
+        Move
+          .where(name: move_json[:name])
+          .first_or_create!(
+            type: cached_types[move_json[:type]],
+            power: move_json[:power],
+            pp: move_json[:pp],
+            accuracy: move_json[:accuracy],
+            effect_chance: move_json[:effect_chance],
+            effect: effect
+          )
+
+      PokemonMove.create!(
+        pokemon: pokemon,
+        move: move,
+        move_method: move_method,
+        level: move_json[:level]
+      )
     end
   end
 end
