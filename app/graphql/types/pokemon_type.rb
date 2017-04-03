95c1a916828cad4236a18baef1aa2f8f9e02f90c
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 17:51:53 -0700

    Add stat columns to pokemon type

--- app/graphql/types/pokemon_type.rb
+++ app/graphql/types/pokemon_type.rb
@@ -4,6 +4,12 @@ Types::PokemonType = GraphQL::ObjectType.define do
   field :id, !types.ID
   field :name, !types.String
   field :number, !types.String
+  field :attack, !types.Int
+  field :defense, !types.Int
+  field :hp, !types.Int
+  field :specialAttack, !types.Int, property: :special_attack
+  field :specialDefense, !types.Int, property: :special_defense
+  field :speed, !types.Int
 
   field :types, !types[!types.String] do
     description 'Pokemon types'
