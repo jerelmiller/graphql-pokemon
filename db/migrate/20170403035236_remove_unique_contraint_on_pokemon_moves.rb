df9bafcf8f6ea4540147a35ce6763df18a8da30e
Jerel Miller <jerelmiller@gmail.com>
2017-04-02 22:40:17 -0700

    Fix migration that removes index

--- db/migrate/20170403035236_remove_unique_contraint_on_pokemon_moves.rb
+++ db/migrate/20170403035236_remove_unique_contraint_on_pokemon_moves.rb
@@ -1,5 +1,12 @@
 class RemoveUniqueContraintOnPokemonMoves < ActiveRecord::Migration[5.0]
-  def change
+  def up
     remove_index :pokemon_moves, name: 'index_pokemon_move_pokemon_move'
+  end
+
+  def down
+    add_index :pokemon_moves,
+      [:pokemon_id, :move_id, :move_method_id],
+      unique: true,
+      name: 'index_pokemon_move_pokemon_move'
   end
 end
