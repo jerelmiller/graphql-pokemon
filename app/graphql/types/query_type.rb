bb33c1696451c9bd1a685ed3ff5e3f53f88ccc30
Jerel Miller <jerelmiller@gmail.com>
2017-04-08 00:40:57 -0700

    Add ability to fetch pokemon by id

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -5,4 +5,10 @@ Types::QueryType = GraphQL::ObjectType.define do
     description 'Get all pokemon'
     resolve Resolvers::PokemonResolver.new
   end
+
+  field :pokemon, Types::PokemonType do
+    argument :id, !types.ID
+    description 'Get a pokemon by id'
+    resolve ->(_, args, ctx) { Pokemon.find(args[:id]) }
+  end
 end
